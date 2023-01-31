da1e6254eefcd0998b73bcc6fff27c7d90143f0c
Miriam Cordes <miriam.cordes@intive-kupferwerk.com>
2023-01-31 10:03:04 +0100

    use scss

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
 import { handleSubmit } from './js/formHandler'
 
+import './styles/index.scss'
+
 // solution taken from: https://knowledge.udacity.com/questions/793705
 document.getElementById("form")
     .addEventListener("submit", handleSubmit)
